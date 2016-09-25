9f250229d42b2cee1127064e8222b44316951871
lesion <lesion@autistici.org>
2016-09-25 12:58:59 +0200

    usage with node

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // require remotestoragejs
-require('remotestoragejs/release/stable/remotestorage.amd.js')
+var RemoteStorage = require('remotestoragejs')
 
 var rs = new RemoteStorage()
-rs.displayWidget()
+//rs.displayWidget()
 rs.access.claim('bookmarks','rw')
 
-require('remotestoragejs-modules/dist/bookmarks')
+require('remotestorage-module-bookmarks')
 var bookmark = {url: 'http://remotestorage.io', title: 'title'}
 rs.bookmarks.archive.store(bookmark)
 
