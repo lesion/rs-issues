03b52b88357dade2c03d780673712b4ef3ec3844
lesion <lesion@autistici.org>
2016-09-25 12:55:06 +0200

    symlink to node_modules so I can include it from html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-var webpack = require("webpack");
- 
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js'
   },
-  devtool: 'source-map',
-  externals: [ {xmlhttprequest: "{XMLHttpRequest:XMLHttpRequest}"} ]
+  externals: { xmlhttprequest: 'XMLHttpRequest'},
 }
