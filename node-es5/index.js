071103e18fe8ea102fcaee631daaf55ebf645663
lesion <lesion@autistici.org>
2016-09-26 16:40:17 +0200

    using new module definition

--- node-es5/index.js
+++ node-es5/index.js
@@ -1,11 +1,11 @@
 // require remotestoragejs
 var RemoteStorage = require('remotestoragejs')
+require('remotestorage-module-bookmarks')(RemoteStorage)
 
 var rs = new RemoteStorage()
 //rs.displayWidget()
 rs.access.claim('bookmarks','rw')
 
-require('remotestorage-module-bookmarks')
 var bookmark = {url: 'http://remotestorage.io', title: 'title'}
 rs.bookmarks.archive.store(bookmark)
 
--- webpack-browser-es6/index.js
+++ webpack-browser-es6/index.js
@@ -1,11 +1,12 @@
 // require remotestoragejs
 import RemoteStorage from 'remotestoragejs'
+import Bookmarks from 'remotestorage-module-bookmarks'
+Bookmarks(RemoteStorage)
 
 const rs = new RemoteStorage()
 rs.displayWidget()
 rs.access.claim('bookmarks','rw')
 
-import bookmarks from 'remotestorage-module-bookmarks'
 var bookmark = {url: 'http://remotestorage.io', title: 'title'}
 rs.bookmarks.archive.store(bookmark)
 
