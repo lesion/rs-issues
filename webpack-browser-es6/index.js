9479dd2dea40e326329c01bdee00dde8b6725bcb
lesion <lesion@autistici.org>
2016-11-01 02:44:50 +0100

    testing new remotestorage-widget

--- webpack-browser-es6/index.js
+++ webpack-browser-es6/index.js
@@ -1,11 +1,13 @@
 // require remotestoragejs
 import RemoteStorage from 'remotestoragejs'
+import bookmarks from 'remotestorage-module-bookmarks'
 import 'remotestorage-widget'
 
+bookmarks(RemoteStorage)
 const rs = new RemoteStorage()
 rs.displayWidget()
-rs.access.claim('bookmarks','rw')
 
+rs.access.claim('bookmarks','rw')
 var bookmark = {url: 'http://remotestorage.io', title: 'title'}
 rs.bookmarks.archive.store(bookmark)
 
